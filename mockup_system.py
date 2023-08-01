68e6e44f861af9e83a6dfbbee6afe3125ab10b58
Samuel Rooney <the.sam.rooney@gmail.com>
2023-07-31 22:12:57 -0400

    create mockup system container class

---
+++ mockup_system.py
@@ -0,0 +1,20 @@
+
+
+
+class MockupSystem:
+    """
+    The MockupSystem is a container class for different Subsystems, which are interconnected modules.
+    These modules may be part of the satellite, or they may represent an element of the environment.
+    This class provides a structure for these modules so they can be reliably found and accessed.
+    It also provides an easy way to pull data and call functions, creating a useful developer interface.
+    """
+
+    subsystems = {}
+
+    def __init__(self):
+        pass
+
+    def update(self, deltaTimeSeconds: float = 1.0):
+        for subsystem in self.subsystems.keys():
+            subsystem.update(deltaTimeSeconds)
+
