1f22a434ab3c124090d8d12f91a4ad330b6e6d67
Samuel Rooney <the.sam.rooney@gmail.com>
2023-07-31 23:35:44 -0400

    create IBattery interface

---
+++ subsystems/IBattery.py
@@ -0,0 +1,22 @@
+
+class IBattery:
+    """
+    Battery class interface. Defines functions and handles any battery must implement/use in order to be used in the mockup.
+    """
+    sources = []
+    sinks = []
+
+    def update(self, deltaTimeSeconds: float = 1.0):
+        raise NotImplementedError("Interface function")
+    
+    def connectPowerSource(self, source):
+        self.sources.append(source)
+    
+    def connectPowerSink(self, sink):
+        self.sinks.append(sink)
+    
+    def getCurrentStoredWattHours(self):
+        raise NotImplementedError("Interface function")
+
+    def getMaximumStoredWattHours(self):
+        raise NotImplementedError("Interface function")
